f86320db175c2cb8b64bfed498312b96aad8dd71
Regilator <reginaldmutungwazi3@gmail.com>
2026-03-27 19:03:52 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const cacheName = 'titan-v3';
-const files = ['./', './index.html', './style.css', './app.js', './catalog.txt', './97093.jpg'];
+const CACHE_NAME = 'titan-omega-final-v1';
+const ASSETS = ['./', './index.html', './style.css', './app.js'];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(files)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
